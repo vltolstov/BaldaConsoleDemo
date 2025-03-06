358ff36d785c0ffd1ce84dc89998b6e00dbfdc28
Vladimir Tolstov <tolstov@gmail.com>
2025-03-06 09:50:18 +0700

    Добавлен метод загадывающий слово

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,14 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     private static String START_COMMAND = "старт";
-    private static ArrayList<String> WORD;
+    private static ArrayList<Character> WORD;
+    private static int WORD_SIZE = 3;
+    private static String OPEN_LETTERS;
     private static boolean GAME_STATUS;
     private static int ERROR_COUNT;
 
@@ -12,7 +17,9 @@ public class Main {
     public static void main(String[] args) {
         do {
             gameInitialize();
-            //тут сама игра
+            if (GAME_STATUS) {
+                setWord();
+            }
         } while (GAME_STATUS);
     }
 
@@ -22,8 +29,25 @@ public class Main {
         if(commandFromUserInput.equals(START_COMMAND)){
             GAME_STATUS = true;
             ERROR_COUNT = 0;
+            WORD = new ArrayList<>();
         } else {
             GAME_STATUS = false;
         }
+    }
+
+    public static void setWord(){
+        //слово без пробелов, не пустое, >= 3 символа
+        System.out.println("Загадайте слово (не менее " + (WORD_SIZE - 1) + " букв)");
+        String userWord = "";
+        while (userWord.isEmpty() || userWord.contains(" ") || userWord.length() < WORD_SIZE){
+            userWord = scanner.nextLine().toLowerCase();
+            if(userWord.isEmpty() || userWord.contains(" ") || userWord.length() < WORD_SIZE){
+                System.out.println("Неподходящее слово. Загадайте другое");
+            }
+        }
+        for(int i = 0; i < userWord.length(); i++){
+            WORD.add(i, userWord.charAt(i));
+        }
+        System.out.println(WORD.toString());
     }
 }
